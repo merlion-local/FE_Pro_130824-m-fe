f90aa9bf654b9d88c2b0017c315ba75174326aa9
Mikhail Smokotnin <75911434+merlion-local@users.noreply.github.com>
2024-12-19 23:37:21 +0400

    Add files via upload

---
+++ lesson_001/vite-project/src/CityPage/CityPage.jsx
@@ -0,0 +1,27 @@
+import 'react'
+import style from './CityPage.module.css'
+
+export default function CityPage(){
+    return(
+        <div className={style.cityClass}>
+            <div className={style.h1_city}>
+                <h1>Все о городе</h1>
+            </div>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                 Maxime tempora quisquam voluptatem officia nostrum aliquam neque,
+                 debitis facere veritatis quas laboriosam! Amet consectetur dolores sit
+                  doloremque architecto iste quam animi.</p>
+                  <img src="https://storage.picsave.pp.ua/cluster1/origin/261208e6ed7ffe6e701b60b6f555081c.jpg" alt="" />
+        </div>
+
+        
+    )
+}
+
+
+
+
+
+
+
